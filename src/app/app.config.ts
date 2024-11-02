7f6bd8f6a5002a86a940705a1b01bccee69d0c8d
rijaramalanjaona <rija.ramalanjaona@gmail.com>
2024-11-02 14:53:04 +0100

    add page-not-found component

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,11 +2,13 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter, Routes } from '@angular/router';
 import { PokemonListComponent } from './pokemon/pokemon-list/pokemon-list.component';
 import { PokemonProfileComponent } from './pokemon/pokemon-profile/pokemon-profile.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const routes: Routes = [
   { path: 'pokemons/:id', component: PokemonProfileComponent },
   { path: 'pokemons', component: PokemonListComponent },
-  { path: '', redirectTo: '/pokemons', pathMatch: 'full' }
+  { path: '', redirectTo: '/pokemons', pathMatch: 'full' },
+  { path: '**', component: PageNotFoundComponent }
 ];
 
 export const appConfig: ApplicationConfig = {
