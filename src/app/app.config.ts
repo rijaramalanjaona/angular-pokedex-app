ef2678a0162824bcddeef9487a48ce222e4d2132
rijaramalanjaona <rija.ramalanjaona@gmail.com>
2024-11-04 22:15:27 +0100

    add pokemon-edit component

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,8 +3,10 @@ import { provideRouter, Routes } from '@angular/router';
 import { PokemonListComponent } from './pokemon/pokemon-list/pokemon-list.component';
 import { PokemonProfileComponent } from './pokemon/pokemon-profile/pokemon-profile.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { PokemonEditComponent } from './pokemon/pokemon-edit/pokemon-edit.component';
 
 const routes: Routes = [
+  { path: 'pokemons/edit/:id', component: PokemonEditComponent, title: 'Pokémon' },
   { path: 'pokemons/:id', component: PokemonProfileComponent, title: 'Pokémon' },
   { path: 'pokemons', component: PokemonListComponent, title: 'Pokédex' },
   { path: '', redirectTo: '/pokemons', pathMatch: 'full' },
---
+++ src/app/pokemon/pokemon-edit/pokemon-edit.component.ts
@@ -0,0 +1,19 @@
+import { Component, inject, signal } from '@angular/core';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+import { PokemonService } from '../../pokemon.service';
+
+@Component({
+  selector: 'app-pokemon-edit',
+  standalone: true,
+  imports: [
+    RouterLink
+  ],
+  templateUrl: './pokemon-edit.component.html',
+  styles: ``
+})
+export class PokemonEditComponent {
+  readonly route = inject(ActivatedRoute);
+  readonly pokemonService = inject(PokemonService);
+  readonly pokemonId = Number(this.route.snapshot.paramMap.get('id'));
+  readonly pokemon = signal(this.pokemonService.getPokemonById(this.pokemonId)).asReadonly();
+}
