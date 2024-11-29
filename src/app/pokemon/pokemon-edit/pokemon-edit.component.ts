d9ce69be3239b1f197c5d7e1fc068afe7a50150f
rijaramalanjaona <rija.ramalanjaona@gmail.com>
2024-11-29 17:52:03 +0100

    Validators on formControl name

--- src/app/pokemon/pokemon-edit/pokemon-edit.component.ts
+++ src/app/pokemon/pokemon-edit/pokemon-edit.component.ts
@@ -1,8 +1,8 @@
 import { Component, inject, signal } from '@angular/core';
 import { ActivatedRoute, RouterLink } from '@angular/router';
 import { PokemonService } from '../../pokemon.service';
-import { FormArray, FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
-import { getPokemonColor, getPokemonTextColor } from '../../pokemon.model';
+import { FormArray, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { getPokemonColor, getPokemonTextColor, POKEMON_RULES } from '../../pokemon.model';
 
 @Component({
   selector: 'app-pokemon-edit',
@@ -18,7 +18,12 @@ export class PokemonEditComponent {
   readonly pokemon = signal(this.pokemonService.getPokemonById(this.pokemonId)).asReadonly();
 
   readonly form = new FormGroup({
-    name: new FormControl(this.pokemon().name),
+    name: new FormControl(this.pokemon().name, [
+      Validators.required,
+      Validators.minLength(POKEMON_RULES.MIN_NAME),
+      Validators.maxLength(POKEMON_RULES.MAX_NAME),
+      Validators.pattern(POKEMON_RULES.NAME_PATTERN),
+    ]),
     life: new FormControl(this.pokemon().life),
     damage: new FormControl(this.pokemon().damage),
     types: new FormArray(
@@ -57,4 +62,9 @@ export class PokemonEditComponent {
   getPokemonTextColor(type: string): string {
     return getPokemonTextColor(type);
   }
+
+  get pokemonName() {
+    return this.form.get('name') as FormControl;
+  }
+
 }
