9ffe6e82a9e732795f42ab98b508ffdd8820d7d5
rijaramalanjaona <rija.ramalanjaona@gmail.com>
2024-12-17 22:44:00 +0100

    pokemon detail from http request

--- src/app/pokemon/pokemon-profile/pokemon-profile.component.ts
+++ src/app/pokemon/pokemon-profile/pokemon-profile.component.ts
@@ -1,7 +1,9 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, inject, Signal } from '@angular/core';
 import { ActivatedRoute, RouterLink } from '@angular/router';
 import { PokemonService } from '../../pokemon.service';
 import { DatePipe } from '@angular/common';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { Pokemon } from '../../pokemon.model';
 
 @Component({
   selector: 'app-pokemon-profile',
@@ -14,5 +16,5 @@ export class PokemonProfileComponent {
   readonly route = inject(ActivatedRoute);
   readonly pokemonService = inject(PokemonService);
   readonly pokemonId = Number(this.route.snapshot.paramMap.get('id'));
-  readonly pokemon = signal(this.pokemonService.getPokemonById(this.pokemonId)).asReadonly();
+  readonly pokemon: Signal<Pokemon | undefined> = toSignal(this.pokemonService.getPokemonById(this.pokemonId));
 }
