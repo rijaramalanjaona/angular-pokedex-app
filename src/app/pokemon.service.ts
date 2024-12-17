6cdf896ac34d5d90dffe0d79b290117600139c12
rijaramalanjaona <rija.ramalanjaona@gmail.com>
2024-12-17 22:21:19 +0100

    getPokemonById use http

--- src/app/pokemon.service.ts
+++ src/app/pokemon.service.ts
@@ -1,6 +1,5 @@
 import { inject, Injectable } from '@angular/core';
 import { Pokemon, PokemonList } from './pokemon.model';
-import { POKEMON_LIST } from './pokemon-list.fake';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
@@ -15,12 +14,8 @@ export class PokemonService {
     return this.http.get<PokemonList>(this.POKEMON_API_URL);
   }
 
-  getPokemonById(id: number): Pokemon {
-    const pokemon = POKEMON_LIST.find(pokemon => pokemon.id === id);
-    if (!pokemon) {
-      throw new Error(`Pokemon not found with id ${id}`);
-    }
-    return pokemon;
+  getPokemonById(id: number): Observable<Pokemon> {
+    return this.http.get<Pokemon>(`${this.POKEMON_API_URL}/${id}`);
   }
 
   getPokemonTypeList(): string[] {
