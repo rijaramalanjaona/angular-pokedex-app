a9b71d139964579661116a63ccdfc9955fe371c2
rijaramalanjaona <rija.ramalanjaona@gmail.com>
2024-12-21 17:31:13 +0100

    manage HTTP error

--- src/app/pokemon/pokemon-profile/pokemon-profile.component.ts
+++ src/app/pokemon/pokemon-profile/pokemon-profile.component.ts
@@ -1,9 +1,10 @@
-import { Component, inject, Signal } from '@angular/core';
+import { Component, computed, inject, Signal } from '@angular/core';
 import { ActivatedRoute, RouterLink } from '@angular/router';
 import { PokemonService } from '../../pokemon.service';
 import { DatePipe } from '@angular/common';
 import { toSignal } from '@angular/core/rxjs-interop';
 import { Pokemon } from '../../pokemon.model';
+import { catchError, map, of } from 'rxjs';
 
 @Component({
   selector: 'app-pokemon-profile',
@@ -16,5 +17,18 @@ export class PokemonProfileComponent {
   readonly route = inject(ActivatedRoute);
   readonly pokemonService = inject(PokemonService);
   readonly pokemonId = Number(this.route.snapshot.paramMap.get('id'));
-  readonly pokemon: Signal<Pokemon | undefined> = toSignal(this.pokemonService.getPokemonById(this.pokemonId));
+
+  private readonly pokemonResponse = toSignal(
+    this.pokemonService.getPokemonById(this.pokemonId)
+      .pipe(
+        map((value: Pokemon) => ({ value, error: undefined })),
+        catchError((error) => of({ value: undefined, error }))
+      )
+  );
+
+  readonly loading: Signal<boolean> = computed(() => !this.pokemonResponse());
+
+  readonly error = computed(() => this.pokemonResponse()?.error);
+
+  readonly pokemon = computed(() => this.pokemonResponse()?.value);
 }
