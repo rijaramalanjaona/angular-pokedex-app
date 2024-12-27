da5392728bb550ef740878830530573ba975251f
rijaramalanjaona <rija.ramalanjaona@gmail.com>
2024-12-27 22:35:19 +0100

    create pokemon-add component

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -7,6 +7,7 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
 import { PokemonEditComponent } from './pokemon/pokemon-edit/pokemon-edit.component';
 import { AuthGuard } from './core/auth/auth.guard';
 import { LoginComponent } from './login/login.component';
+import { PokemonAddComponent } from './pokemon/pokemon-add/pokemon-add.component';
 
 const routes: Routes = [
   {
@@ -22,6 +23,11 @@ const routes: Routes = [
         path: '',
         component: PokemonListComponent,
         title: 'Pokédex',
+      },
+      {
+        path: 'add',
+        component: PokemonAddComponent,
+        title: 'Pokémon',
       },
       {
         path: 'edit/:id',
---
+++ src/app/pokemon/pokemon-add/pokemon-add.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pokemon-add',
+  standalone: true,
+  imports: [],
+  templateUrl: './pokemon-add.component.html',
+  styles: ``
+})
+export class PokemonAddComponent {
+
+}
